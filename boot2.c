2a863e6aaf888ae44da2733014352a12fe1823c4
David Albert <davidbalbert@gmail.com>
2015-09-16 20:48:51 -0400

    Use 28-bit LBA. No need for more in the bootloader

--- boot2.c
+++ boot2.c
@@ -13,12 +13,12 @@
 #define COMMAND 0x1F7
 #define STATUS 0x1F7 /* Same as COMMAND */
 
-void readlba(void *, ulong, ushort);
+void readlba(void *, uint, uchar);
 
 void
 bootmain(void)
 {
-    readlba(0x100000, 1, 1);
+    readlba((void *)0x100000, 1, 1);
 
     for (;;)
         hlt();
@@ -28,35 +28,24 @@ bootmain(void)
 void
 waitdisk(void)
 {
-  while((inb(0x1F7) & 0xC0) != 0x40)
+    while((inb(0x1F7) & 0xC0) != 0x40)
         ;
 }
 
 void
-readlba(void *addr, ulong start, ushort count)
+readlba(void *addr, uint lba, uchar count)
 {
     int i;
     ushort data;
 
     waitdisk();
 
-    // Select LBA mode (are we also selecting the primary drive?)
-    outb(DRIVE, 0x40);
-
-    // For performance, don't send writes to the same IO port in a row.
-    outb(COUNT, count >> 8);
-
-    outb(LOW, start >> 24 & 0xFF); // Byte 4 (1 indexed)
-    outb(MID, start >> 32 & 0xFF); // Byte 5
-    outb(HIGH, start >> 40 & 0xFF); // Byte 6
-
-    outb(COUNT, count & 0xFF);
-
-    outb(LOW, start & 0xFF); // Byte 1
-    outb(MID, start >> 8 & 0xFF); // Byte 2
-    outb(HIGH, start >> 16 & 0xFF); // Byte 3
-
-    outb(COMMAND, 0x24); // 0x24 == READ SECTORS EXT
+    outb(0x1F6, 0xE0 | (lba >> 24));
+    outb(0x1F2, count);
+    outb(0x1F3, lba);
+    outb(0x1F4, lba >> 8);
+    outb(0x1F5, lba >> 16);
+    outb(0x1F7, 0x20);  // READ SECTORS
 
     waitdisk();
 
