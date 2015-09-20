0dbcc0e6d3bfa9597c7457d0dab349d00254967c
David Albert <davidbalbert@gmail.com>
2015-09-19 23:02:58 -0400

    Now booting really works

--- main.c
+++ main.c
@@ -3,29 +3,34 @@
 #include "mem.h"
 #include "x86.h"
 
-void
-kmain(void)
+int
+main(void)
 {
-    *((ushort *)0xB8000) = 0x4020;
+    *((ushort *)0xB8000) = 0x5020;
 
     for (;;)
         hlt();
 }
 
 
+
+// Paging structures used to map first 2MB at low and high
+// addresses after we get control from the bootloader (see
+// start.S)
 __attribute__((__aligned__(PGSIZE)))
-pde bootpgdirhigh[NPDENTRIES];
+pml4e bootpml4[NPDENTRIES];
+
 
 __attribute__((__aligned__(PGSIZE)))
 pdpe bootpdphigh[NPDENTRIES];
 
-
 __attribute__((__aligned__(PGSIZE)))
-pde bootpgdirlow[NPDENTRIES];
+pde bootpgdirhigh[NPDENTRIES];
+
 
 __attribute__((__aligned__(PGSIZE)))
 pdpe bootpdplow[NPDENTRIES];
 
 __attribute__((__aligned__(PGSIZE)))
-pml4e bootpml4[NPDENTRIES];
+pde bootpgdirlow[NPDENTRIES];
 
