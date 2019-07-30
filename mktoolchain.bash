816d7de74d56eab787a9e2c0d6b401959309a774
David Albert <davidbalbert@gmail.com>
2019-07-30 18:42:38 -0400

    GCC 9.1.0 and Binutils 2.32, etc.

--- mktoolchain.bash
+++ mktoolchain.bash
@@ -8,10 +8,10 @@ fi
 set -e
 
 GMP=https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
-MPFR=https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.1.tar.bz2
+MPFR=https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.2.tar.bz2
 MPC=https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
-BINUTILS=https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.bz2
-GCC=https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz
+BINUTILS=https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.bz2
+GCC=https://ftp.gnu.org/gnu/gcc/gcc-9.1.0/gcc-9.1.0.tar.gz
 
 function envfile() {
   pushd $(dirname $0) >/dev/null
