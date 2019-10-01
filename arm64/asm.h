ac1ada5653aff54adb583bc5845bf7543a527724
David Albert <davidbalbert@gmail.com>
2019-10-01 08:46:35 -0400

    finish el2 switch

--- arm64/asm.h
+++ arm64/asm.h
@@ -6,3 +6,9 @@
 #define SCR_EL3_HCE (1 << 8)    // Hypervisor Call instruction enable - enables HVC instruction
 #define SCR_EL3_SMD (1 << 7)    // Secure Monitor Call disable - disables SMC instruction
 #define SCR_EL3_NS  1           // Non-secure bit - EL0 and EL1 are not in Secure state
+
+#define SPSR_EL3_M_EL2H 0b1001   // retrun to EL2h mode - EL2 with EL2 stack (handler stack)
+#define SPSR_EL3_F  (1 << 6)     // FIQ mask bit
+#define SPSR_EL3_I  (1 << 7)     // IRQ mask bit
+#define SPSR_EL3_A  (1 << 8)     // SError mask bit
+#define SPSR_EL3_D  (1 << 9)     // Debug exception mask bit
