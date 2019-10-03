36453987d142bd7b311c217e1e130e265a5a8d33
David Albert <davidbalbert@gmail.com>
2019-10-03 09:03:46 -0400

    Use EL1h instead of EL1t

--- arm64/asm.h
+++ arm64/asm.h
@@ -7,10 +7,16 @@
 #define SCR_EL3_HCE (1 << 8)    // Hypervisor Call instruction enable - enables HVC instruction
 #define SCR_EL3_RW  (1 << 10)   // EL2 is 64 bit, EL0 and EL1 can be 64 bit
 
-#define SPSR_EL3_M_EL2H 0b1001   // retrun to EL2h mode - EL2 with EL2 stack (handler stack)
+#define SPSR_EL3_M_EL2H 0b1001   // return to EL2h mode - EL2 with EL2 stack (handler stack)
 #define SPSR_EL3_F  (1 << 6)     // FIQ mask bit
 #define SPSR_EL3_I  (1 << 7)     // IRQ mask bit
 #define SPSR_EL3_A  (1 << 8)     // SError mask bit
 #define SPSR_EL3_D  (1 << 9)     // Debug exception mask bit
 
 #define HCR_EL2_RW  (1 << 31)    // EL1 is 64 bit, EL0 can be 64 bit
+
+#define SPSR_EL2_M_EL1H 0b0101   // return to EL1h mode
+#define SPSR_EL2_F  (1 << 6)     // FIQ mask
+#define SPSR_EL2_I  (1 << 7)     // IRQ mask
+#define SPSR_EL2_A  (1 << 8)     // SError mask
+#define SPSR_EL2_D  (1 << 9)     // Debug mask
