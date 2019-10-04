3128aa7c69357769574af6439f7634229dfcdae5
David Albert <davidbalbert@gmail.com>
2019-10-04 07:58:08 -0400

    Disable HVC

--- arm64/asm.h
+++ arm64/asm.h
@@ -4,7 +4,6 @@
 
 #define SCR_EL3_NS  1           // Non-secure bit - EL0 and EL1 are not in Secure state
 #define SCR_EL3_SMD (1 << 7)    // Secure Monitor Call disable - disables SMC instruction
-#define SCR_EL3_HCE (1 << 8)    // Hypervisor Call instruction enable - enables HVC instruction
 #define SCR_EL3_RW  (1 << 10)   // EL2 is 64 bit, EL0 and EL1 can be 64 bit
 
 #define SPSR_EL3_M_EL2H 0b1001   // return to EL2h mode - EL2 with EL2 stack (handler stack)
@@ -14,6 +13,7 @@
 #define SPSR_EL3_D  (1 << 9)     // Debug exception mask bit
 
 #define HCR_EL2_RW  (1 << 31)    // EL1 is 64 bit, EL0 can be 64 bit
+#define HCR_EL2_HCD (1 << 29)    // Disables HVC instruction
 
 #define SPSR_EL2_M_EL1H 0b0101   // return to EL1h mode
 #define SPSR_EL2_F  (1 << 6)     // FIQ mask
