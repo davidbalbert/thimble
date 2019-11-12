e277ad3460100b34e22412bd1c929e9dff06d29e
David Albert <davidbalbert@gmail.com>
2019-11-12 15:07:48 -0500

    Printhello system call works

--- arm64/syscall.c
+++ arm64/syscall.c
@@ -2,9 +2,33 @@
 
 #include "arm64.h"
 #include "defs.h"
+#include "proc.h"
+#include "syscall.h"
+
+long sys_printhello(void);
+
+static long (*syscalls[])(void) = {
+    [SYS_PRINTHELLO] sys_printhello,
+};
+
+long
+sys_printhello(void)
+{
+    cprintf("Hello!\n");
+
+    return 0;
+}
 
 void
 syscall(TrapFrame *tf)
 {
-    cprintf("syscall\n");
+    int num = tf->x8;
+
+    if (num > 0 && num < nelem(syscalls) && syscalls[num]) {
+        proc->tf = tf;
+        tf->x0 = syscalls[num]();
+    } else {
+        cprintf("unknown syscall: %d\n", num);
+        tf->x0 = -1;
+    }
 }
